0d251551957766c6e69bc9214e8505a26e4931d8
Kiran Kumar Pradhan <pradhankukiran@gmail.com>
2026-04-03 20:54:52 +0530

    Wire Wasm host: DI registration, App.razor routing, index.html

--- src/RequestKit.Wasm/Program.cs
+++ src/RequestKit.Wasm/Program.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RequestKit.Core.Services;
+using RequestKit.Core.State;
+using RequestKit.Shared.Interop;
 using RequestKit.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// State
+builder.Services.AddSingleton<AppState>();
+builder.Services.AddSingleton<ApiClientState>();
+
+// JS Interop
+builder.Services.AddSingleton<MonacoEditorInterop>();
+builder.Services.AddSingleton<KeyboardInterop>();
+builder.Services.AddSingleton<FileInterop>();
+builder.Services.AddSingleton<HttpFetchInterop>();
+builder.Services.AddSingleton<IWorkspaceStorageService, IndexedDbWorkspaceStorage>();
 
 await builder.Build().RunAsync();
